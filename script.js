2f2ee3fadfbba2a6e0285e9ad586bf14ed19bc49
sammiefrog <sammiefrog@live.com>
2020-02-21 21:46:34 -0600

    adding an object

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+var securePassword = {
+  numberOfLetters : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28],
+  lowerCase : true,
+  upperCase : true,
+  numberEls : true,
+  specialChar : true,
 
 // Write password to the #password input
 function writePassword() {
@@ -8,35 +17,39 @@ function writePassword() {
   passwordText.value = password;
 
   function passLength() {
-    var numberOfLetters = prompt("How many letters do you want in your password? (1-28)")
-  
-    if (i >= 1 && i <= 28) {
-      then ("Thank You!")
+
+    if (numberOfLetters >= 1 && numberOfLetters <= 28) {
+      alert("Thank You!")
     }
     else {
       alert("Please enter a valid number!")
     }
   }
+  console.log(passLength())
+
     // Functions, CharCode (http://net-comber.com/charset.html)
-  function lowerCase (){
-  return string.fromCharCode (Math.floor(Math.random() * 26) + 97);
-  }
-  console.log(lowerCase()); 
-
-  function upperCase (){
-      return string.fromCharCode (Math.floor(Math.random() * 26) + 65);
+  function lowerCaseFunc (){
+      return string.fromCharCode(Math.floor(Math.random() * 26) + 97);
       }
-  console.log(upperCase()); 
+  console.log(lowerCaseFunc()); 
 
-  function numberEls (){
-      return string.fromCharCode (Math.floor(Math.random() * 10) + 48);
+  function upperCaseFunc (){
+      return string.fromCharCode(Math.floor(Math.random() * 26) + 65);
       }
-  console.log(numberEls()); 
+  console.log(upperCaseFunc()); 
 
-  function specialChar (){
-      return string.fromCharCode (Math.floor(Math.random() * 15) + 33);
+  function numberElsFunc (){
+      return string.fromCharCode(Math.floor(Math.random() * 10) + 48);
       }
-  console.log(specialChar());
+  console.log(numberElsFunc()); 
+
+  function specialCharFunc (){
+      return string.fromCharCode(Math.floor(Math.random() * 15) + 33);
+      }
+  console.log(specialCharFunc());
+
+}
+
 }
 
 // Add event listener to generate button
@@ -49,4 +62,3 @@ generateBtn.addEventListener("click", () => {
 
 });
 
-(writePassword);
