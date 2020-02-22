a2f2b1326ba68755aaec230c61e27025e749c0e2
sammiefrog <sammiefrog@live.com>
2020-02-22 16:31:43 -0600

    added for loops to push strings into new array

--- script.js
+++ script.js
@@ -23,6 +23,8 @@ function generatePassword() {
   var numberCase = confirm("Do you want to include numbers?");
   var specialChar = confirm("Do you want to include special characters?");
 
+  var passCriteria = [];
+
   //number of characters wanted
   if (passLength >= 8 && passLength <= 128) {
     passLengthValue = parseInt(passLength);
@@ -31,34 +33,50 @@ function generatePassword() {
     alert("Please enter a valid number!")
   }
 
+
   //lowercase letters
     if (lowerCase===true) {
       lowerCaseValue = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+        for (i = 0; i > lowerCaseValue.length; i++) {
+          passCriteria.push(lowerCaseValue[i]);
+        }
     }
 
   //uppercase letters
     if (upperCase===true) {
       upperCaseValue = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+        for (i = 0; i > upperCaseValue.length; i++) {
+          passCriteria.push(upperCaseValue[i]);
+      }
     }
 
   //numbers 
     if (numberCase===true) {
       numberCaseValue = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+      for (i = 0; i > numberCaseValue.length; i++) {
+        passCriteria.push(numberCaseValue[i]);
+      }
     }
 
   //special characters 
     if (specialChar===true) {
       specialCharValue = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ":", ";", "'", "<", ">", "?", "/", "`", "~"];
+
+      for (i = 0; i > specialCharValue.length; i++) {
+        passCriteria.push(specialCharValue[i]);
+      }
     }
 
-    var passCriteria = [lowerCaseValue + upperCaseValue + numberCaseValue + specialCharValue]
     var newPassword = ""
 
-    for (var i = passCriteria.length; i < passLengthValue; i++) {
-      newPassword += Math.floor(Math.random() * passCriteria.length);
+    for (var i = 0; i < passLengthValue; i++) {
+      newPassword += passCriteria[Math.floor(Math.random() * passCriteria.length)];
     }
 
-    console.log(newPassword);
+    return newPassword;
 
 }
 
