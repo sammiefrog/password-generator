5ec0785e775c015057b6990ea958b7985741c0c1
sammiefrog <sammiefrog@live.com>
2020-02-22 14:45:31 -0600

    added a for loop

--- script.js
+++ script.js
@@ -2,7 +2,11 @@
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var lowerCaseValue;
+var upperCaseValue;
+var numberCaseValue;
+var specialCharValue;
+var passLengthValue;
 
 function writePassword() {
 
@@ -13,45 +17,54 @@ function writePassword() {
 }
 
 function generatePassword() {
-  var numberOfLetters = prompt("How many letters do you want in your password? (1-28)");
-  var lowerCase = confirm("Do you want to include lowercase letters?")
-  var upperCase = confirm("Do you want to include uppercase letters?")
-  var numberCase = confirm("Do you want to include numbers?")
-  var specialChar = confirm("Do you want ot include special characters?")
+  var passLength = prompt("How many letters do you want in your password? (8-128)");
+  var lowerCase = confirm("Do you want to include lowercase letters?");
+  var upperCase = confirm("Do you want to include uppercase letters?");
+  var numberCase = confirm("Do you want to include numbers?");
+  var specialChar = confirm("Do you want to include special characters?");
 
-  
-    if (numberOfLetters >= 8 && numberOfLetters <= 128) {
-      numberOfLettersValue = parseInt(numberOfLetters);
-    }
-    else {
-      alert("Please enter a valid number!")
-    }
-  
+  //number of characters wanted
+  if (passLength >= 8 && passLength <= 128) {
+    passLengthValue = parseInt(passLength);
+  }
+  else {
+    alert("Please enter a valid number!")
+  }
+
+  //lowercase letters
     if (lowerCase===true) {
       lowerCaseValue = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     }
 
+  //uppercase letters
     if (upperCase===true) {
       upperCaseValue = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     }
 
+  //numbers 
     if (numberCase===true) {
       numberCaseValue = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
     }
 
+  //special characters 
     if (specialChar===true) {
       specialCharValue = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ":", ";", "'", "<", ">", "?", "/", "`", "~"];
     }
 
+    var passCriteria = [lowerCaseValue + upperCaseValue + numberCaseValue + specialCharValue]
+    var newPassword = ""
+
+    for (var i = passCriteria.length; i < passLengthValue; i++) {
+      newPassword += Math.floor(Math.random() * passCriteria.length);
+    }
+
+    console.log(newPassword);
 
 }
 
-// }
-
-//
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", () => {
-  (writePassword);
+  writePassword;
 });
 
